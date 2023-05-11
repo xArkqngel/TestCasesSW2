9cfa3e48361e255ba14f2f3f1d907ed4aabb02bd
sofiiass <83323215+sofiiass@users.noreply.github.com>
2023-05-11 13:46:35 -0500

    Update person.test.js

--- src/tests/person.test.js
+++ src/tests/person.test.js
@@ -37,8 +37,8 @@ describe('Test for Register', () => {
 
     test('Register Successful', async () => {
         const user = {
-            name: 'Julio',
-            email: 'julio@gmail.com',
+            name: 'Julia',
+            email: 'julia@gmail.com',
             password: 'Password123'
         };
         const result = await registerF(user);
